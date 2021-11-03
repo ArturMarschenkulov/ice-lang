b9f845d288f91365cf99eca9cc1eee7e3d197971
ArturMarschenkulov <ArturMarschals@web.de>
2021-11-03 11:35:26 +0100

    second commit

--- main.cpp
+++ main.cpp
@@ -1,23 +1,10 @@
 #include <iostream>
 #include <string>
 #include "llvm/IR/LLVMContext.h"
-#include "Token.h"
-
-
-
 
 
 int main() {
-
-
     std::cout << "This is the start of ice-lang" << std::endl;
-
-
     llvm::LLVMContext TheContext;
-
-    Token token;
-    token.type = TOKEN_TYPE::KW_RETURN;
-
-    std::cout << to_string(token.type) << std::endl;
     return 0;
 }
